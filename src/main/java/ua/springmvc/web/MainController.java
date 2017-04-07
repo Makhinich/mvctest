7cbde10f14823562b8859f7da24740079fddae89
Makhinich <slav.m82@gmail.com>
2017-04-07 22:14:29 +0300

    pom

--- src/main/java/ua/springmvc/web/MainController.java
+++ src/main/java/ua/springmvc/web/MainController.java
@@ -2,8 +2,8 @@ package ua.springmvc.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.xml.ws.RequestWrapper;
 
 /**
  * Created by Slava on 07.04.2017.
@@ -12,7 +12,7 @@ import javax.xml.ws.RequestWrapper;
 @Controller
 public class MainController {
 
-    @RequestMapping (value = "/")
+    @RequestMapping (value = "/", method = RequestMethod.GET)
     public String index () {
         return "index";
     }
